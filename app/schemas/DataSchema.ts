030b6a72fdaec5823e96b3f6244ee3aa01b92fce
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2018-07-27 00:46:45 -0400

    Add method of adding custom serialization logic to mongoose schemas

--- app/schemas/DataSchema.ts
+++ app/schemas/DataSchema.ts
@@ -1,12 +1,15 @@
-import {Document, Schema, SchemaDefinition, SchemaOptions} from "mongoose";
+import {Document, Schema, SchemaDefinition, SchemaOptions} from 'mongoose';
 
-export default function (definition?: SchemaDefinition, options?: SchemaOptions): Schema {
+export default function (definition?: SchemaDefinition, options?: SchemaOptions, toJSON?: (_doc: Document, ret: any) => any): Schema {
     const schema = new Schema(definition, options);
     schema.set('toJSON', {
         transform(_doc: Document, ret: any) {
             ret.id = ret._id;
             delete ret._id;
             delete ret.__v;
+            if (toJSON) {
+                toJSON(_doc, ret);
+            }
         }
     });
     return schema;
