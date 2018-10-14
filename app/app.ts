9893bffb11d3216c5163949e9a72aa8b637adf52
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2018-10-14 15:05:22 -0400

    Remove CORS header

--- app/app.ts
+++ app/app.ts
@@ -13,14 +13,6 @@ import {SitemapController} from './routes/SitemapController';
 
 const app: Application = express();
 
-//Enable cors
-app.use(function (_req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token, *');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS, *');
-    next();
-});
-
 app.use(logger('dev'));
 
 app.use(cookieParser());
